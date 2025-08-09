6b45d53dcb7d3eee798b3ccebcf042a29b4b2acb
Arthur Silva <arthur.silva443@gmail.com>
2025-08-09 16:38:32 -0300

    fix(env): Corrige ambiente de desenvolvimento e adiciona AuthGuard

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 // Interface para tipar a resposta do token
 interface AuthResponse {
@@ -14,9 +15,23 @@ export class AuthService {
   // A URL da nossa API
   private apiUrl = 'https://localhost:7130/api/auth';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   login(credentials: any): Observable<AuthResponse> {
     return this.http.post<AuthResponse>(`${this.apiUrl}/login`, credentials);
+  }
+
+  logout(): void {
+    localStorage.removeItem('authToken');
+    this.router.navigate(['/login']);
+  }
+
+  isAuthenticated(): boolean {
+    const token = localStorage.getItem('authToken');
+    return !!token;
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('authToken');
   }
 }
